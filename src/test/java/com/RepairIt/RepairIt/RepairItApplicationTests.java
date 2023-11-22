5ae7260d1ed3b65473135962e9a1ba190eeeccb8
ethe <ethemanuelivan@gmail.com>
2023-11-22 16:37:36 +0100

    integration

---
+++ src/test/java/com/RepairIt/RepairIt/RepairItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RepairIt.RepairIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
